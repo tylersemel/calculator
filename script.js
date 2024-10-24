d60ffc1979a60ee5ae763a77bd0fd9f26db1b067
tylersemel <tylersemel@gmail.com>
2024-10-24 12:35:04 -0400

    Display a solution based on the equals function

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let firstNum = '';
 let operator = '';
 let secondNum = '';
+let solution = 0;
 let display = document.querySelector(".display");
+let operatorDisplay = document.querySelector(".operator-display");
 const numbers = document.querySelector(".numbers");
 const functions = document.querySelector(".functions");
+const equals = document.querySelector(".equals");
 
 let displayValue = 0;
 let afterOperator = false;
@@ -27,46 +30,93 @@ function divide(a, b) {
 function operate(operator, a, b) {
     switch (operator) {
         case '+':
-            add(a, b);
+            solution = add(a, b);
             break;
         case '-':
-            subtract(a, b);
+            solution = subtract(a, b);
             break;
         case '*':
-            multiply(a, b);
+            solution = multiply(a, b);
             break;
         case '/':
-            divide(a, b);
+            solution = divide(a, b);
             break;
         default:
             console.log("Incorrect operator");
     }
 }
 
-function populateDisplay(value) {
-    displayValue = parseInt(value);
-    display.textContent = parseInt(value);
+function setDisplay(value) {
+    displayValue = value;
+    display.textContent = value;
 }
 
 numbers.addEventListener("click", (e) => {
     if (!afterOperator) {
         firstNum += e.target.textContent;
-        populateDisplay(firstNum);
+        setDisplay(firstNum);
     }
     else {
         secondNum += e.target.textContent;
-        populateDisplay(secondNum);
+        setDisplay(secondNum);
+        setOperatorDisplay('');
     }
 });
 
+function setOperator(operatorVal) {
+    switch (operatorVal) {
+        case '+':
+            operator = '+';
+            break;
+        case '-':
+            operator = '-';
+            break;
+        case '×':
+            operator = '*';
+            break;
+        case '÷':
+            operator = '/';
+            break;
+        default:
+            console.log("Shouldnt be here");
+    }
+}
 
+function setOperatorDisplay(operator) {
+    operatorDisplay.textContent = operator;
+}
 
 //where to get the operator
 functions.addEventListener("click", (e) => {
     if (firstNum == '') {
         return;
     }
-
     
-    populateDisplay(e.target.textContent);
-})
+    setOperator(e.target.textContent);
+    setOperatorDisplay(operator);
+    // populateDisplay(e.target.textContent);
+    afterOperator = true;
+    // operate(operator, firstNum, secondNum);
+
+    console.log(operator);
+});
+
+function convertToInt(num) {
+    return parseInt(num);
+}
+
+equals.addEventListener("click", () => {
+    if (firstNum === '' || secondNum === '') {
+        return;
+    }
+
+    convertToInt(firstNum);
+    convertToInt(secondNum);
+
+    operate(operator, firstNum, secondNum);
+    setDisplay(solution);
+
+    // afterOperator = false;
+    firstNum = solution;
+    secondNum = '';
+});
